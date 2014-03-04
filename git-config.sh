1f82e51cd3c197176f6d1866196a7ea26735bc72
Jeremy Laflin <laflijj@gmail.com>
2014-03-04 14:13:54 -0500

    remove gitconfig.link and .template replaced with git-config.sh

--- git-config.sh
+++ git-config.sh
@@ -1,3 +1,4 @@
 git config --global user.name "Jeremy Laflin"
 git config --global user.email "laflijj@gmail.com"
 git config --global credential.helper osxkeychain
+git config --global core.excludesfile ~/Software/system-config/dotfiles/git/.gitignore_global
